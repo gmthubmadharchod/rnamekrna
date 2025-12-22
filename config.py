88560d202a0d7d31e50a9ce9b45b59dd354addbb
Abhinai Reddy⛔️ <159715958+abhinai2244@users.noreply.github.com>
2025-12-22 09:51:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "")
     API_HASH  = os.environ.get("API_HASH", "")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    STRING_SESSION = os.environ.get("STRING_SESSION", "")
     PORT = os.environ.get("PORT", "8980")
 
     # database config
